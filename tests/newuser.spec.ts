e57543b5091f74c1c85a3643f2e0fb520601aaf9
Waseem Akbar <waz7861988@hotmail.com>
2023-06-08 15:40:26 +0200

    Add comment sign in not working in headless mode

--- tests/newuser.spec.ts
+++ tests/newuser.spec.ts
@@ -14,40 +14,40 @@ test('Registration page exists', async ({ page }) => {
 
 test('Login with new user to validate user creation', async ({page}) => {
 
-    //unable to get feedback that the registration is complete? In which case I have to do a login to check it is created successfully
-    await page.goto('https://advantageonlineshopping.com/#/register');
+  //unable to get feedback that the registration is complete? In which case I have to do a login to check it is created successfully
+  await page.goto('https://advantageonlineshopping.com/#/register');
 
-    //account details
-    await page.locator('input[name="usernameRegisterPage"]').type(userName);
-    await page.locator('input[name="emailRegisterPage"]').type('test@test.com');
-    await page.locator('input[name="passwordRegisterPage"]').type('Testing1');
-    await page.locator('input[name="confirm_passwordRegisterPage"]').type('Testing1');
-  
-    //personal details
-    await page.locator('input[name="first_nameRegisterPage"]').type('Test');
-    await page.locator('input[name="last_nameRegisterPage"]').type('User');
-    await page.locator('input[name="phone_numberRegisterPage"]').type('99999999');
-  
-    //address
-    await page.getByRole('listbox').selectOption('object:234');
-    await page.locator('input[name="cityRegisterPage"]').fill('Trondheim');
-    await page.locator('input[name="addressRegisterPage"]').fill('Sunny Way');
-    await page.locator('input[name="state_\\/_province_\\/_regionRegisterPage"]').fill('Trøndelag');
-    await page.locator('input[name="postal_codeRegisterPage"]').fill('7000');
-  
-    //agree to terms, disagree with offers
-    await page.locator('input[name="i_agree"]').check();
-    await page.locator('input[name="allowOffersPromotion"]').uncheck();
-  
-    //register
-    await page.getByRole('button', { name: 'REGISTER' }).click();
+  //account details
+  await page.locator('input[name="usernameRegisterPage"]').type(userName);
+  await page.locator('input[name="emailRegisterPage"]').type('test@test.com');
+  await page.locator('input[name="passwordRegisterPage"]').type('Testing1');
+  await page.locator('input[name="confirm_passwordRegisterPage"]').type('Testing1');
+
+  //personal details
+  await page.locator('input[name="first_nameRegisterPage"]').type('Test');
+  await page.locator('input[name="last_nameRegisterPage"]').type('User');
+  await page.locator('input[name="phone_numberRegisterPage"]').type('99999999');
+
+  //address
+  await page.getByRole('listbox').selectOption('object:234');
+  await page.locator('input[name="cityRegisterPage"]').fill('Trondheim');
+  await page.locator('input[name="addressRegisterPage"]').fill('Sunny Way');
+  await page.locator('input[name="state_\\/_province_\\/_regionRegisterPage"]').fill('Trøndelag');
+  await page.locator('input[name="postal_codeRegisterPage"]').fill('7000');
+
+  //agree to terms, disagree with offers
+  await page.locator('input[name="i_agree"]').check();
+  await page.locator('input[name="allowOffersPromotion"]').uncheck();
+
+  //register
+  await page.getByRole('button', { name: 'REGISTER' }).click();
 
   //login with the new user
   await page.goto('https://advantageonlineshopping.com/#/');
   await page.click("#menuUser")
   await page.locator('input[name="username"]').type(userName);
   await page.locator('input[name="password"]').type('Testing1');
-  await page.locator('#sign_in_btnundefined').click();
+  await page.locator('#sign_in_btnundefined').click(); // does not seem to be working in headless mode
 
   //check that the new user is logged in
   await expect(page.locator('#menuUserLink > span')).toHaveText(userName);
