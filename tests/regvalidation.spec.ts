eea731cadd78b0ef4f0bb6141992ba0e06bc8f91
Waseem Akbar <waz7861988@hotmail.com>
2023-06-12 21:45:28 +0200

    Add two test step examples to validation

--- tests/regvalidation.spec.ts
+++ tests/regvalidation.spec.ts
@@ -3,15 +3,19 @@ import { test, expect } from '@playwright/test';
 test('Registration page validation', async ({ page }) => {
     await page.goto('https://advantageonlineshopping.com/#/register');
   
-    await page.locator('input[name="usernameRegisterPage"]').type('aaa');
-    await page.locator('input[name="emailRegisterPage"]').click();
-    await expect(page.getByText('Use 5 character or longer', { exact: true })).toBeVisible;
-    //"Use 5 characters or longer" this should read.
+
+    await test.step(`Check username`, async () => {
+        await page.locator('input[name="usernameRegisterPage"]').type('aaa');
+        await page.locator('input[name="emailRegisterPage"]').click();
+        await expect(page.getByText('Use 5 character or longer', { exact: true })).toBeVisible;
+        //"Use 5 characters or longer" this should read.
+    });
     
-    await page.locator('input[name="emailRegisterPage"]').type('aaa');
-    await page.locator('input[name="passwordRegisterPage"]').click();;
-    await page.getByText('Use 5 character or longer', { exact: true }).click();
-    //"Your email address is not formatted correctly"
+    await test.step(`Check email address`, async () => {
+        await page.locator('input[name="emailRegisterPage"]').type('aaa');
+        await page.locator('input[name="passwordRegisterPage"]').click();;
+        await expect(page.getByText("Your email address is not formatted correctly", { exact: true })).toBeVisible;
+        //"Your email address is not formatted correctly"
+    });
     
-    await page.locator('input[name="usernameRegisterPage"]').clear();
 })
